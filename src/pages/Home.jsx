28ace6b80644912b1fb6ca9170d307f713eb5a97
Deepak <deep84chaudhary@gmail.com>
2023-09-26 21:02:36 +0530

    Added home page and used and used fakerapi to render the e-commerce products to give the feel and demonstrate the e-commerce website

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,44 @@
-import HomeLayout from "../layouts/PageLayout";
-function Home() {
+import React, { useEffect, useState } from "react";
 
-    return (
-        <HomeLayout>
-            <div className="min-h-[90vh] flex items-center justify-center">
-                Home Page
+import HomeLayout from "../layouts/PageLayout";
+
+function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  async function getProducts() {
+    try {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const products = await response.json();
+      setProducts(products);
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  }
+
+  return (
+    <HomeLayout>
+      <div className="min-h-[90vh] flex flex-wrap items-center justify-center gap-8 bg-[#f1f3f6] w-[98%] py-16">
+        {products.map((product) => (
+          <div className="w-[250px] h-[350px] m-2 p-2 text-center rounded-lg bg-white shadow-2xl" key={product.id}>
+            <div className="flex items-center justify-center h-[180px]">
+                <img src={product.image} alt={product.title} className="w-[180px] h-[180px] object-contain cursor-pointer" />
             </div>
-        </HomeLayout>
-    )
+            <h3 className="whitespace-nowrap overflow-hidden overflow-ellipsis pt-5">{product.title}</h3>
+            <p className="text-[#008000]">Price ${product.price}</p>
+            <div className="flex items-center justify-around">
+                <button className="mt-8 bg-black text-white border-none py-1 w-24 cursor-pointer rounded-lg">Add to Cart</button>
+                <button className="mt-8 bg-black text-white border-none py-1 w-24 cursor-pointer rounded-lg">Buy Now</button>
+            </div>
+         </div>
+        
+        ))}
+      </div>
+    </HomeLayout>
+  );
 }
 
 export default Home;
