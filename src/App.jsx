fe05feb80a6a4de22821f19d840b81d253bf779c
Deepak <deep84chaudhary@gmail.com>
2023-09-28 01:22:38 +0530

    Added PageNotFound page

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Product from "./pages/Product";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/product/:id" element={<Product />}/>
+      <Route path="*" element={<PageNotFound />}/>
     </Routes>
   );
 }
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import pageNotFound from "../assets/Images/pageNotFound.jpg"
+
+function PageNotFound() {
+    
+    const navigate = useNavigate();
+    function goback() {
+        navigate(-1)
+    };
+
+    return (
+        <div className="flex items-center justify-center h-screen w-full">
+            <div>
+                <img src={pageNotFound} alt="404-img" />
+            </div>
+            <div className="absolute bottom-16" onClick={goback}>
+                <button className="border bg-[#8bc34a] w-[10rem] px-3 py-3 rounded-md font-bold text-lg text-black cursor-pointer hover:bg-[#388e3c] transition-all ease-in-out duration-300">Go Back</button>
+            </div>
+        </div>
+    )
+}
+
+export default PageNotFound;
