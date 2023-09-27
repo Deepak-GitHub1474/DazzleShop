2976223cd39437ae41ad74078b66b1c2005ecc75
Deepak <deep84chaudhary@gmail.com>
2023-09-27 16:57:05 +0530

    made home page responsive to adopt in type of screen size device

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,38 +28,39 @@ function Home() {
   return (
     <HomeLayout>
         <div className="flex flex-col w-full bg-[#f1f3f6] mt-6">
-            <div className="carousel w-1/2 mx-auto">
+            <div className="carousel w-full lg:w-1/2 mx-auto">
                 <div id="slide1" className="carousel-item relative w-full">
-                    <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                        <img src={carouselImg1} className="w-[600px] h-[300px]" />
-                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <div className="flex flex-col items-center justify-center gap-4 px-4 lg:px-[15%]">
+                        <img src={carouselImg1} className="w-full lg:w-[600px] h-[300px]" alt="Slide 1" />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-0 right-0 mx-auto top-1/2">
+                            {/* Todo fix next btn */}
                             <a href="#slide4" className="btn btn-circle bg-[#8bc34a] shadow-lg shadow-gray-600/50">❮</a>
-                            <a href="#slide2" className="btn btn-circle bg-[#8bc34a] shadow-lg shadow-gray-600/50">❯</a>
+                            <a href="#slide2" className="btn btn-circle bg-[#8bc34a] shadow-lg shadow-gray-600/50">❯</a> 
                         </div>
                     </div>
                 </div>
                 <div id="slide2" className="carousel-item relative w-full">
-                    <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                        <img src={carouselImg2} className="w-[600px] h-[300px]" />
-                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <div className="flex flex-col items-center justify-center gap-4 px-4 lg:px-[15%]">
+                        <img src={carouselImg2} className="w-full lg:w-[600px] h-[300px]" alt="Slide 2" />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-0 right-0 mx-auto top-1/2">
                             <a href="#slide1" className="btn btn-circle bg-[#8bc34a] shadow-lg shadow-gray-600/50">❮</a>
                             <a href="#slide3" className="btn btn-circle bg-[#8bc34a] shadow-lg shadow-gray-600/50">❯</a>
                         </div>
                     </div>
                 </div>
                 <div id="slide3" className="carousel-item relative w-full">
-                    <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                        <img src={carouselImg3} className="w-[600px] h-[300px]" />
-                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <div className="flex flex-col items-center justify-center gap-4 px-4 lg:px-[15%]">
+                        <img src={carouselImg3} className="w-full lg:w-[600px] h-[300px]" alt="Slide 3" />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-0 right-0 mx-auto top-1/2">
                             <a href="#slide2" className="btn btn-circle bg-[#8bc34a] shadow-lg shadow-gray-600/50">❮</a>
                             <a href="#slide4" className="btn btn-circle bg-[#8bc34a] shadow-lg shadow-gray-600/50">❯</a>
                         </div>
                     </div>
                 </div>
                 <div id="slide4" className="carousel-item relative w-full">
-                    <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                        <img src={carouselImg4} className="w-[600px] h-[300px]" />
-                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <div className="flex flex-col items-center justify-center gap-4 px-4 lg:px-[15%]">
+                        <img src={carouselImg4} className="w-full lg:w-[600px] h-[300px]" alt="Slide 4" />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-0 right-0 mx-auto top-1/2">
                             <a href="#slide3" className="btn btn-circle bg-[#8bc34a] shadow-lg shadow-gray-600/50">❮</a>
                             <a href="#slide1" className="btn btn-circle bg-[#8bc34a] shadow-lg shadow-gray-600/50">❯</a>
                         </div>
@@ -67,34 +68,38 @@ function Home() {
                 </div>
             </div>
         </div>
-        <div className=" flex items-center justify-center gap-10 bg-[#f1f3f6] py-10 mt-6">
-            <div className="w-[50%] flex items-center justify-center ">
-                <img src={homePageMainImage} alt="home page" className="w-[88%] rounded-[100px]" />
+
+        
+        <div className="flex flex-col lg:flex-row items-center justify-center gap-8 bg-[#f1f3f6] py-10 mt-6">
+            <div className=" flex items-center justify-center">
+                <img src={homePageMainImage} alt="home page" className="w-[90%] lg:w-[70%] md:w-[55%] sm:w-[70%] rounded-[100px]" />
             </div>
 
-            <div className="w-[50%] space-y-6">
+            <div className="sm:w-1/2 space-y-6 mx-5">
                 <img src={homeLeaf} alt="homeLeaf-Img" />
-                <h3 className="text-2xl font-semibold">Best Quality Products</h3>
-                <div className="text-5xl font-bold max-w-lg">
+                <h3 className="text-xl sm:text-2xl font-semibold">Best Quality Products</h3>
+                <div className="text-2xl sm:text-4xl xl:text-4xl 2xl:text-5xl font-bold">
                     <h1>Join The Online Dazzle</h1>
                     <span className="text-[#8bc34a] font-bold">Shop Movement!</span>
                 </div>
-                <p className="text-lg text-gray-600  max-w-xl">
-                Discover the Best Quality Products and Join the Dazzling Online Shopping Movement! Shop with us for top-notch items that bring style and quality to your life.
+                <p className="text-base lg:text-lg text-gray-600 max-w-xl">
+                    Discover the Best Quality Products and Join the Dazzling Online Shopping Movement! Shop with us for top-notch items that bring style and quality to your life.
                 </p>
             </div>
         </div>
+
       <div className="flex flex-wrap items-center justify-center gap-8 bg-[#f1f3f6] py-6 my-6 ">
         {products.map((product) => (
-          <div className="w-[250px] h-[350px] m-2 p-2 text-center rounded-lg bg-[#ffffff] shadow-2xl" key={product.id}>
+          <div className="sm:w-[250px] w-[200px] h-[330px] m-2 p-2 text-center rounded-lg bg-[#ffffff] shadow-2xl hover:scale-[1.02] hover:border-2 hover:border-[#8bc34a]" key={product.id}>
             <div className="flex items-center justify-center h-[180px]">
                 <img src={product.image} alt={product.title} className="w-[180px] h-[180px] object-contain cursor-pointer" />
             </div>
             <h3 className="whitespace-nowrap overflow-hidden overflow-ellipsis pt-5 text-[#000] font-semibold">{product.title}</h3>
             <p className="text-[#008000] font-semibold">Price ${product.price}</p>
             <div className="flex items-center justify-around">
-                <button className="mt-8 bg-[#000] text-[#ffffff] font-semibold border-none py-1 w-24 cursor-pointer rounded-lg">Add to Cart</button>
-                <button className="mt-8 bg-[#000] text-[#ffffff] font-semibold border-none py-1 w-24 cursor-pointer rounded-lg">Buy Now</button>
+                <button className="mt-6 bg-[#000] text-[#ffffff] font-semibold border-none py-1 sm:w-24 w-[85px] sm:text-base text-sm cursor-pointer rounded-lg">Add to Cart</button>
+
+                <button className="mt-6 bg-[#000] text-[#ffffff] font-semibold border-none py-1 sm:w-24 w-[85px] sm:text-base text-sm cursor-pointer rounded-lg">Buy Now</button>
             </div>
       </div>
         
