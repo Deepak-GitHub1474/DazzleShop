5f57caef0338292990315b975bda9ea316b98562
Deepak <deep84chaudhary@gmail.com>
2023-10-09 17:13:58 +0530

    updated some css in Home, Category page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -35,20 +35,20 @@ function Home() {
 
         <div className="flex flex-wrap items-center justify-center gap-8 py-6 my-6 mx-4">
           {products.map((product) => (
-            <div className="sm:w-[250px] sm:h-[320px] w-[100px] h-[170px] sm:m-2 p-2 text-center rounded-lg bg-[#ffffff] shadow-2xl hover:scale-[1.02] hover:border-2 hover:border-[#8bc34a]" key={product.id}>
+            <div className="sm:w-[230px] sm:h-[330px] w-[150px] h-[235px] sm:m-2 p-3 text-center rounded-lg bg-[#ffffff] shadow-2xl hover:scale-[1.02] hover:border-2 hover:border-[#8bc34a]" key={product.id}>
                 <Link to={`/product/${product.id}`}>
-                    <div className="flex items-center justify-center sm:h-[180px] h-[70px]">
-                        <img src={product.image} alt={product.title} className="sm:w-[180px] sm:h-[180px] w-[50px] h-[50px] object-contain cursor-pointer" />
+                    <div className="flex items-center justify-center sm:h-[180px] h-[100px]">
+                        <img src={product.image} alt={product.title} className="sm:w-[180px] sm:h-[180px] w-[90px] h-[90px] object-contain cursor-pointer" />
                     </div>
                 </Link>
-                <h3 className="whitespace-nowrap overflow-hidden overflow-ellipsis sm:pt-5  text-[#000] sm:font-semibold sm:text-base text-[10px]">{product.title}</h3>
-                <p className="text-[#008000] sm:font-semibold sm:text-base text-[10px]">Price ${Math.round(product.price)}</p>
+                <h3 className="whitespace-nowrap overflow-hidden overflow-ellipsis sm:pt-5 pt-1 text-[#000] font-semibold sm:text-base text-sm">{product.title}</h3>
+                <p className="text-[#008000] font-semibold sm:text-base text-[12px] mt-1">Price ${Math.round(product.price)}</p>
                 <div className="sm:flex sm:items-center sm:justify-around sm:flex-row flex flex-col justify-center items-center">
                     
-                    <button onClick={() => addToCart(product.id - 1)} className="sm:mt-4 mt-2 bg-[#000] text-[#ffffff] sm:font-semibold border-none sm:py-1 py-[2px] text-center sm:w-24 w-[85px] sm:text-base text-[10px] cursor-pointer sm:rounded-lg rounded-md">Add to Cart</button>
+                    <button onClick={() => addToCart(product.id - 1)} className="sm:mt-4 mt-2 bg-[#000] text-[#ffffff] font-semibold border-none py-[5px] text-center w-24 sm:text-base text-[10px] cursor-pointer sm:rounded-lg rounded-md hover:bg-[#8bc34a]">Add to Cart</button>
 
                     <Link to={`/product/${product.id}`}>
-                        <button className="sm:mt-4 mt-2 bg-[#000] text-[#ffffff] sm:font-semibold border-none sm:py-1 py-[2px] text-center sm:w-24 w-[85px] sm:text-base text-[10px] cursor-pointer sm:rounded-lg rounded-md">Buy Now</button>
+                        <button className="sm:mt-4 mt-2 bg-[#000] text-[#ffffff] font-semibold border-none py-[5px] text-center w-24 sm:text-base text-[10px] cursor-pointer sm:rounded-lg rounded-md hover:bg-[#8bc34a]">Buy Now</button>
                     </Link>
                 </div>
             </div>
